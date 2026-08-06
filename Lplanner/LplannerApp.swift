72e7e1f0a172fd59ad27c5dc08563f238e362751
carlos lander <carloslander@etik.com>
2026-08-06 13:03:54 -0400

    Initial Commit

---
+++ Lplanner/LplannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LplannerApp.swift
+//  Lplanner
+//
+//  Created by Carlos Lander on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct LplannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LplannerTests/LplannerTests.swift
@@ -0,0 +1,18 @@
+//
+//  LplannerTests.swift
+//  LplannerTests
+//
+//  Created by Carlos Lander on 8/6/26.
+//
+
+import Testing
+
+struct LplannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
